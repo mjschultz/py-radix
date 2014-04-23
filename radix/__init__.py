e417368419fde2310e0c20a0488c3223c73512bb
Michael J. Schultz <mjschultz@gmail.com>
2014-04-23 11:38:53 -0500

    Bump version: 0.5.0 → 0.6.0

--- radix/__init__.py
+++ radix/__init__.py
@@ -3,7 +3,7 @@ try:
 except Exception as e:
     from .radix import Radix as _Radix
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __all__ = ['Radix']
 
 
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if not IS_PYPY and not RADIX_NO_EXT:
 
 setup(
     name='py-radix',
-    version='0.5.0',
+    version='0.6.0',
     maintainer='Michael J. Schultz',
     maintainer_email='mjschultz@gmail.com',
     url='https://github.com/mjschultz/py-radix',
