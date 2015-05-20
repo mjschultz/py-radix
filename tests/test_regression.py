61bf810bde8777f9613b9350e5b09af1228a602a
Henrik Thostrup Jensen <htj@nordu.net>
2015-05-20 14:17:59 +0200

    name test properly

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -431,7 +431,7 @@ class TestRadix(unittest.TestCase):
             ['10.0.0.0/8', '10.0.0.0/13', '10.0.0.0/31', '11.0.0.0/16'])
 
 
-    def test_26_search_covered_segfault(self):
+    def test_27_search_covered_segfault(self):
         # the following will make py-radix 0.8 segfault
         tree = radix.Radix()
         tree.add('193.178.156.0/24')
